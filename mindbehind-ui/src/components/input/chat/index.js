f2df6b38446591ccae6ddb0298463b8df0c64796
ByrMucahit <m.mucahitbayar@gmail.com>
2022-11-18 08:45:30 +0300

    Add message Context

--- mindbehind-ui/src/components/input/chat/index.js
+++ mindbehind-ui/src/components/input/chat/index.js
@@ -1,23 +1,40 @@
 import {Input} from 'antd'
 import {HeartOutlined, PaperClipOutlined, CompassOutlined, SendOutlined} from '@ant-design/icons'
 import styles from './style.module.css'
+import {useState} from "react";
+
+
 
 const ChatInput = () => {
 
-    return(
+    const [content, setContent] = useState();
+    const [value, setValue] = useState("");
+
+    const sendMessage = () => {
+        console.log('Hello World' + value.target.value);
+    }
+
+    return (
         <div className={styles.inputContainer}>
             <div className={styles.inputField}>
-                <Input placeholder="Please text your message" className={styles.inputComponent}/>
-                <SendOutlined />
+                <Input placeholder="Please text your message" className={styles.inputComponent}
+                       onChange={(val) =>  setValue(val)}/>
+                <div className={styles.sendIconContainer}>
+                    <button onClick={sendMessage}>
+                        <SendOutlined/>
+                    </button>
+
+                </div>
+
             </div>
 
             <div className={styles.iconContainer}>
-                <HeartOutlined />
-                <PaperClipOutlined />
-                <CompassOutlined />
+                <HeartOutlined/>
+                <PaperClipOutlined/>
+                <CompassOutlined/>
             </div>
         </div>
     )
 }
 
-export  default ChatInput;
+export default ChatInput;
